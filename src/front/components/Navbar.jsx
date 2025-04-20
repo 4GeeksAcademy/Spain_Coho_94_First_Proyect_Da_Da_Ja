5c15622c17dc838f46dbb5b7ee851479953c1f9f
Dani Yanani <alejandrorot2@gmail.com>
2025-04-20 15:17:13 +0000

    mejoras varias

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useNavigate, Link } from "react-router-dom";
 import "./Styles/Navbar.css";
-import LogoFrame from "./Logo";
 import { useTheme } from '../Contexts/ThemeContext.jsx';
 import { useState, useEffect } from "react";
 
@@ -8,77 +7,99 @@ const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
   const navigate = useNavigate();
 
-  // Manejo reactivo del estado de login
+  // Cambio del boton Login a Logout
+
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("access_token"));
 
-  // FUNCION DEL BOTON LOGOUT
+
+  // -----------------------BOTON LOGOUT-------------------------
+
   const LogoutButton = () => {
     localStorage.removeItem("access_token");
-    console.log('Token eliminado:', localStorage.getItem("access_token")); // aviso en consola del token eliminado
-    setIsLoggedIn(false); // Actualiza el estado después de hacer logout
+    console.log('Token eliminado:', localStorage.getItem("access_token")); 
+
+    setIsLoggedIn(false);
     navigate("/login");
   };
 
-  // Monitorear cambios en localStorage y actualizar el estado
+  // busca en localStorage y actualizar el estado
   useEffect(() => {
     const checkLoginStatus = () => {
-      // Actualiza el estado de login directamente desde localStorage
+
+      
       setIsLoggedIn(!!localStorage.getItem("access_token"));
     };
 
-    // Inicializa el estado al cargar el componente
+    // Inicializa el estado cuando carga o cambia el token
     checkLoginStatus();
 
-    // Escuchar cambios en localStorage
+    // Listener para avisar de los cambios en localStorage
+
     window.addEventListener("storage", checkLoginStatus);
-    
-    // Limpiar el listener cuando el componente se desmonte
+
+    // Limpiar el listener cuando el componente se quita
     return () => {
       window.removeEventListener("storage", checkLoginStatus);
     };
-  }, []); // Solo se ejecuta una vez al montar el componente
+  }, []);
 
   return (
     <nav>
       <div className="nav-content">
-        <div className="logo">
-          <LogoFrame />
-        </div>
+        
+        {/* a la izquierda los botones de navegación */}
 
-        <div className="nav_buttons">
-          {!isLoggedIn && (
-            <>
-              <Link to="/" className="nav-btn">Register</Link>
-              <Link to="/login" className="nav-btn">Login</Link>
-            </>
-          )}
+        <div className="nav-left">
           <Link to="/home" className="nav-btn">Home</Link>
-          
           {isLoggedIn && (
             <>
               <Link to="/inventory" className="nav-btn">Inventario</Link>
               <Link to="/admin/store-settings" className="nav-btn">Datos del Comercio</Link>
               <Link to="/cart" className="nav-btn">Cart</Link>
-              <button className="nav-btn logout-btn" onClick={LogoutButton}>Cerrar sesión</button>
             </>
           )}
-          
-          {/* BOTÓN DE CAMBIO DE TEMA */}
-          <button
-            onClick={toggleTheme}
-            className="nav-btn theme-btn"
-            style={{
-              backgroundColor: "var(--primary)",
-              color: "white",
-              borderRadius: "5px",
-              padding: "5px 10px",
-            }}
-          >
-            {theme === "light" ? "Oscuro" : "Claro"}
-          </button>
+        </div>
+  
+        {/* en el centro el logo */}
+        <div className="nav-center">
+          <img
+            src="https://raw.githubusercontent.com/4GeeksAcademy/Spain_Coho_94_First_Proyect_Da_Da_Ja/main/src/front/assets/logo.png"
+            alt="Logo"
+            className="logo-img"
+          />
+        </div>
+  
+        {/* a la derech los botones del usuario */}
+        <div className="nav-right">
+          <div className="nav_buttons">
+            {!isLoggedIn ? (
+              <>
+                <Link to="/" className="nav-btn">Register</Link>
+                <Link to="/login" className="nav-btn">Login</Link>
+              </>
+            ) : (
+              <button className="nav-btn logout-btn" onClick={LogoutButton}>
+                Cerrar sesión
+              </button>
+            )}
+  
+            {/* Botón de cambio de tema */}
+            <button
+              onClick={toggleTheme}
+              className="nav-btn theme-btn"
+              style={{
+                backgroundColor: "var(--primary)",
+                color: "white",
+                borderRadius: "5px",
+                padding: "5px 10px",
+              }}
+            >
+              {theme === "light" ? "Oscuro" : "Claro"}
+            </button>
+          </div>
         </div>
       </div>
     </nav>
-  );
-};
-export default Navbar;
+  )
+};  
+   export default Navbar;
